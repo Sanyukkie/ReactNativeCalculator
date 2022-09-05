7e9a4e966357aeadb6fb1f8e8c2cd0dd6281afe8
Aleksander Truss <aleksander.truss@nordhealth.com>
2022-09-05 20:35:11 +0300

    Exercise Complete

--- App.js
+++ App.js
@@ -1,10 +1,49 @@
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TextInput } from 'react-native';
+
+
 
 export default function App() {
+
+  const [num1, setNum1] = useState(0);
+  const [num2, setNum2] = useState(0);
+
+  const [total, setTotal] = useState(0);
+ 
+
+  const addition = () => {
+    setTotal(parseInt(num1)+parseInt(num2))
+  }
+
+  const substraction = () => {
+    setTotal(parseInt(num1)-parseInt(num2))
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Text>Result: {total}</Text>
+      <TextInput
+        style={{width:200, borderColor: 'gray', borderWidth:1}}
+        onChangeText={num1 => setNum1(num1)}
+        value={num1}
+        numeric
+        keyboardType={'numeric'}
+      />
+      <TextInput
+        style={{width:200, borderColor: 'gray', borderWidth:1}}
+        onChangeText={num2 => setNum2(num2)}
+        value={num2}
+        numeric
+        keyboardType={'numeric'}
+      />
+
+      <View style={styles.button_row}>
+        <Button onPress={addition} title="+" />
+        <Button onPress={substraction} title="-" />
+      </View>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +56,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  button_row: {
+    flexDirection: "row",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }
 });
